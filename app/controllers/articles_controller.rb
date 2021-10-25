00a46164dbb1305f1b0d981a879d2d9a493c936c
gauti-tag <gautier.tiehoule@gmail.com>
2021-10-25 10:35:19 +0000

    refactor the code using partials and production ready

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,10 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
     #byebug
-    @article = Article.find(params[:id])
+    
   end
 
   def index 
@@ -14,11 +17,11 @@ class ArticlesController < ApplicationController
   
   def edit 
     #byebug
-   @article = Article.find(params[:id])
+   
   end
 
   def create 
-    @article = Article.new(params.require(:article).permit(:title, :desciption))
+    @article = Article.new(article_params)
      if @article.save
        flash[:notice] = "Article was created successfully."
        redirect_to @article
@@ -28,8 +31,8 @@ class ArticlesController < ApplicationController
   end
 
   def update 
-    @article = Article.find(params[:id])
-      if @article.update(params.require(:article).permit(:title, :desciption))
+  
+      if @article.update(article_params)
         flash[:notice] = "the article was updated."
         redirect_to @article
       else
@@ -38,10 +41,22 @@ class ArticlesController < ApplicationController
   end
 
   def destroy 
-    @article = Article.find(params[:id])
+    
     @article.destroy
     flash[:notice] = "the article was deleted"
     redirect_to articles_path()
   end
+
+
+
+  private
+
+   def set_article
+    @article = Article.find(params[:id])
+   end
+
+   def article_params
+     params.require(:article).permit(:title, :desciption)
+   end
 
 end
