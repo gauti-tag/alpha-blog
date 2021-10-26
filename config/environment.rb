4a1b7697dd41aa718dfb79a3008e319bfe7c3cfe
gauti-tag <gautier.tiehoule@gmail.com>
2021-10-26 12:16:33 +0000

    styling validation and flash messages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# script config for removing the div form the errors messages : field_with_errors
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+   html_tag.html_safe
+end
