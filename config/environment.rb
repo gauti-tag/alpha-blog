4241555b1b43dfe5f71b1d446f966dff51602697
gauti-tag <gautier.tiehoule@gmail.com>
2021-10-26 13:32:25 +0000

    style show view

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# script config for removing the div form the errors messages : field_with_errors
+# script config for removing the div form the errors messages : field_with_errors functiona
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
    html_tag.html_safe
 end
