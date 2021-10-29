05daeb7ec9ff4795af6b1fe54f10425e52ecd8b5
gauti-tag <gautier.tiehoule@gmail.com>
2021-10-29 14:19:02 +0000

    add edit template and edit & update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit 
+    #byebug
+    @user = User.find(params[:id])
+  end
+
+  def update 
+    @user = User.find(params[:id])
+      if @user.update(user_params) 
+       flash[:notice] = "Your account information was successfully updated"
+       redirect_to articles_path 
+      else
+         render 'edit'
+      end
+  end
+
   def create 
     @user = User.new(user_params)
      if @user.save
@@ -21,4 +36,5 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
 
+  
 end
