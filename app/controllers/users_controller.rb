5a9335bda0102f097b2a89a85e539b8fddfd4d72
gauti-tag <gautier.tiehoule@gmail.com>
2021-10-29 16:22:08 +0000

    add index users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
-
+  
   def show 
     @user = User.find(params[:id])
     @articles = @user.articles  # display articles according to the user
+  end
+
+  def index 
+    @users = User.all
   end
 
   def new
